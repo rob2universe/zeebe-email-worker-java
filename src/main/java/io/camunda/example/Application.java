8eb520107a335133ce9d7240c794b7351250d6e2
rob2universe <rob2universe@gmail.com>
2021-11-04 17:14:39 +0800

    initial checkin

---
+++ src/main/java/io/camunda/example/Application.java
@@ -0,0 +1,14 @@
+package io.camunda.example;
+
+import io.camunda.zeebe.spring.client.EnableZeebeClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableZeebeClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/io/camunda/example/worker/SendEmailWorker.java
@@ -0,0 +1,70 @@
+package io.camunda.example.worker;
+
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.client.api.worker.JobHandler;
+import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class SendEmailWorker implements JobHandler {
+
+  public static final String EMAIL_BODY_VAR = "mailBody";
+  public static final String EMAIL_SUBJECT_VAR = "mailSubject";
+  public static final String EMAIL_FROM = "my.worker@camunda.cloud";
+  public static final String EMAIL_TO = "could come from data too";
+
+  public JavaMailSender mailSender;
+
+  public SendEmailWorker(JavaMailSender mailSender) {
+    this.mailSender = mailSender;
+  }
+
+  @Override
+  @ZeebeWorker(type = "emailService")
+  public void handle(JobClient client, ActivatedJob job) {
+
+    try {
+      final Map<String, Object> variables = job.getVariablesAsMap();
+      String emailBody = (String) variables.get(EMAIL_BODY_VAR);
+      if (null==emailBody) throw new RuntimeException("Missing process data: " + EMAIL_BODY_VAR);
+
+      String emailSubject = (String) variables.get(EMAIL_SUBJECT_VAR);
+      if (null==emailSubject) throw new RuntimeException("Missing process data: " + EMAIL_SUBJECT_VAR);
+
+      //send email
+      MimeMessage message =mailSender.createMimeMessage();
+      MimeMessageHelper helper = new MimeMessageHelper(message, false, "utf-8");
+      helper.setTo(EMAIL_TO);
+      helper.setSubject(emailSubject + " (" + job.getKey()+")");
+      helper.setFrom(EMAIL_FROM);
+      message.setContent(emailBody, "text/html");
+      mailSender.send(message);
+
+      // complete the external task
+      client.newCompleteCommand(job.getKey())
+          .variables(Map.of("result", "done"))
+          .send();
+        //.whenComplete((result, exception) -> {log.info("Job completion result: {} : {}", result, exception);});
+      log.info("Task {} with instance id {} for process instance {} completed.",
+          job.getElementInstanceKey(), job.getKey(), job.getProcessInstanceKey());
+
+    } catch (MailException | MessagingException mailEx) {
+      log.error(mailEx.getMessage(), mailEx);
+      // create incident, no retries
+      client.newFailCommand(job.getKey())
+          .retries(0)
+          .errorMessage(mailEx.getMessage())
+          .send();
+    }
+  }
+}
